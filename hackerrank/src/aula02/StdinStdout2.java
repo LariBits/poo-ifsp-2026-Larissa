ed1e5453fca950fd56585e497c690e993836911d
LariSilva <larissasilva8344@gmail.com>
2026-08-12 17:03:33 -0300

    Atividade HackerHanck StdinStdout2

--- hackerrank/src/aula02/StdinStdout2.java
+++ hackerrank/src/aula02/StdinStdout2.java
@@ -1,38 +1,18 @@
 package aula02;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class StdinStdout2 {
-
-    private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
 
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            Scanner scan = new Scanner(System.in);
 
-        if(N % 2 == 0){
+            int i = scan.nextInt();
+            double d = scan.nextDouble();
+            scan.nextLine();
+            String s = scan.nextLine();
 
-            if(N >= 2 && N <= 5){
-                System.out.println("Not Weird");
-            }
-            else if(N >= 6 && N <= 20){
-                System.out.println("Weird");
-            }
-            else if (N > 20){
-                System.out.println("Not Weird");
-            }
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+
         }
-        else{
-            System.out.println("Weird");
-        }
-
-        scanner.close();
     }
-
-}
