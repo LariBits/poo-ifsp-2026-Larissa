d1163ea165cbaafe81880f1ae60d6224c6e508f5
LariSilva <larissasilva8344@gmail.com>
2026-08-12 17:11:49 -0300

    Atividade hackerHanck OutputFormatting

--- hackerrank/src/aula02/OutputFormatting.java
+++ hackerrank/src/aula02/OutputFormatting.java
@@ -1,4 +1,17 @@
 package aula02;
 
 public class OutputFormatting {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+
+    }
 }
